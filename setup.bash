5b0eaed9e2c5472a28a79ce032e979978ac7c02b
Håkon Schia <hakonschia5@gmail.com>
2019-09-03 19:26:43 +0200

    Fixed some shellcheck warnings

--- setup.bash
+++ setup.bash
@@ -12,23 +12,23 @@ uid=$(id -u) # The user ID of the user running the program (used to check for su
 if [ $# -gt 0 ]; then
     case $1 in
         "-d")
-            if [ $uid -eq 0 ]; then # Is ran as root
+            if [ "$uid" -eq 0 ]; then # Is ran as root
                 echo "Downloading.."
 
                 # Download packages
                 apt update && apt upgrade
 
                 apt install gnome-tweaks # Extra settings for gnome
-                echo -e "\n\n\tDownloading 'gnome-tweaks'"
+                echo -e "\\n\\n\\tDownloading 'gnome-tweaks'"
 
                 apt install dconf-editor # A lot of different settings
-                echo -e "\n\n\tDownloading 'dconf-editor'"
+                echo -e "\\n\\n\\tDownloading 'dconf-editor'"
 
                 apt install python3-dev python3-pip python3-setuptools
-                echo -e "\n\n\tDownloading python tools"
+                echo -e "\\n\\n\\tDownloading python tools"
 
                 pip3 install thefuck
-                echo -e "\n\n\tDownloading 'fuck' from https://github.com/nvbn/thefuck"
+                echo -e "\\n\\n\\tDownloading 'fuck' from https://github.com/nvbn/thefuck"
             else
                 echo "Error: -d needs root permissions, run with sudo"
             fi
@@ -40,19 +40,19 @@ bashrc=$(cat ~/.bashrc)
 
 # PROMPT_DIRTRIM not already added
 if [[ $bashrc != *"PROMPT_DIRTRIM"* ]]; then
-    echo -e "PROMPT_DIRTRIM=3\n" >> ~/.bashrc # Sets the terminal prompt to max 3 directories
+    echo -e "PROMPT_DIRTRIM=3\\n" >> ~/.bashrc # Sets the terminal prompt to max 3 directories
 fi
 
 
 dconfInstalled=false
 # If dconf-editor is not installed, ask if the user wants to install it
-if [ $(dpkg-query -W -f='${Status}' dconf-editor 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+if [ "$(dpkg-query -W -f='${Status}' dconf-editor 2>/dev/null | grep -c "ok installed")" -eq 0 ]; then
     echo "dconf-editor is not installed, do you want to install it? (y/n)"
     read -r answer
 
     case $answer in
         "y")
-            if [ $uid -eq 0 ]; then # is root, install dconf-editor
+            if [ "$uid" -eq 0 ]; then # is root, install dconf-editor
                 apt install dconf-editor
                 dconfInstalled=true
             else
@@ -67,7 +67,7 @@ else
 fi
 
 # If dconf-editor is installed change various settings
-if [ $dconfInstalled ]; then
+if [ "$dconfInstalled" ]; then
     # Import terminal profiles from the profiles/ directory
     # Exporting profiles can be done with:
     # dconf dump /org/gnome/terminal/legacy/profiles:/:<profileid>/ > <profileid>
@@ -81,8 +81,8 @@ if [ $dconfInstalled ]; then
         # TODO: Checking against duplicates
         # Will now add duplicate entries to the list, so it will say you have multiple of the same profile
 
-        name=$(basename $profile) # The name of the actual file, ie. the profile ID (not the entire path)
-        dconf load /org/gnome/terminal/legacy/profiles:/:$name/ < $profile
+        name=$(basename "$profile") # The name of the actual file, ie. the profile ID (not the entire path)
+        dconf load /org/gnome/terminal/legacy/profiles:/:"$name"/ < "$profile"
 
 
         profileList="$profileList, '$name'" # Add the profile to the list
@@ -100,6 +100,7 @@ if [ $dconfInstalled ]; then
     # Other values can be found in dconf-editor and by following org.gnome.shell...
     gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'cycle-windows'
     
+    gsettings set org.gnome.desktop.interface clock-show-date true # Show the date (sep. 3 etc.)
     gsettings set org.gnome.desktop.interface clock-show-seconds true # Show seconds in clock on top
     gsettings set org.gnome.desktop.interface clock-show-weekday true # Show what day it is in clock on top (mo, tu etc.)
 fi
@@ -107,12 +108,12 @@ fi
 
 # Add /opt/lampp to path (through bashrc) if it's not there
 # This allows the usage of "xampp start" from anywhere
-if [[ $PATH != *":/opt/lampp"* ]] || [[ $bashrc != *":/opt/lampp"* ]]; then
-    echo -e "export PATH=$PATH:/opt/lampp\n" >> ~/.bashrc
+if [[ "$PATH" != *":/opt/lampp"* ]] || [[ $bashrc != *":/opt/lampp"* ]]; then
+    echo -e "export PATH=$PATH:/opt/lampp\  \n" >> ~/.bashrc
 fi
 
 # To use sudo <command> the exectuable needs to be in a path in secure_path in /etc/sudoers
-if [[ $uid -eq 0 ]]; then # Is root
+if [[ "$uid" -eq 0 ]]; then # Is root
     sudoers=$(cat /etc/sudoers | grep secure_path)
 
     if [[ $sudoers != *":/opt/lampp"* ]]; then
@@ -135,7 +136,7 @@ fi
 
 
 # Installs some gnome tools and echos where to go to install multi monitors add-on
-if [[ $uid -eq 0 ]]; then
+if [[ "$uid" -eq 0 ]]; then
     apt install gnome-tweak-tool
     apt install chrome-gnome-shell
 
@@ -148,12 +149,12 @@ fi
 # '"'"' = end the first string, add a new string with double quotes with a single quote in it
 # concatinate that again with a new string
 # It turns out as: alias please='sudo $(history -p !!)'
-if [[ $bashrc != *"alias please"* ]]; then
+if [[ "$bashrc" != *"alias please"* ]]; then
     # -e to print \n as newline
     echo -e '\nalias please='"'"'sudo $(history -p !!)'"'"'' >> ~/.bashrc
 fi
 # Add alias for both please and pls
-if [[ $bashrc != *"alias pls"* ]]; then
+if [[ "$bashrc" != *"alias pls"* ]]; then
     echo -e '\nalias pls='"'"'sudo $(history -p !!)'"'"'' >> ~/.bashrc
 fi
 
